a9d92dc83bf8d1396ef81c618cd3aab27e8ef5cd
Akil Hamilton <akil.james83@gmail.com>
2019-03-08 23:23:05 -0500

    Edit bash script

--- generate_graph/graph_states/generate.sh
+++ generate_graph/graph_states/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Use FFMPEG to generate MP4
-ffmpeg -framerate 1 -pattern_type glob -i '*.png'   -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
+ffmpeg -framerate 1 -pattern_type glob -i '*.png'   -c:v libx264 -r 30 -pix_fmt yuv420p graph_vid/recent.mp4
